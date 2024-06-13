6551df63544b39723942bf3bd9424096d70baa97
ERICK-KRAMER <kramerfilho@hotmail.com>
2024-06-13 14:32:14 -0300

    add tailwind on project

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import './style.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
