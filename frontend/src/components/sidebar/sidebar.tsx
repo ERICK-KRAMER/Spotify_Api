92cbd68ffba0447bb932a87bd0ff9b2e2e8a52bb
ERICK-KRAMER <kramerfilho@hotmail.com>
2024-06-13 21:18:47 -0300

    style: change theme

--- frontend/src/components/sidebar/sidebar.tsx
+++ frontend/src/components/sidebar/sidebar.tsx
@@ -9,23 +9,44 @@ const Sidebar = () => {
   }
 
   return (
-    <aside className={`w-[300px] bg-red-600 p-4 h-screen flex items-end justify-between gap-4 flex-col transition duration-500 rounded-tr-md ${isOpen ? 'translate-x-0' : '-translate-x-60'} `}>
+    <aside className={`w-[300px] bg-neutral-600 p-2 h-screen flex items-end justify-between gap-4 flex-col transition duration-500 rounded-tr-md 
+    ${isOpen ? 'translate-x-0' : '-translate-x-60'} `}>
+
       {isOpen ?
-        <SquareChevronLeft onClick={handleClick} className="cursor-pointer" />
+        <span className={`cursor-pointer transition duration-300 rounded-full w-10 h-10 flex justify-center items-center 
+        ${isOpen ? 'hover:bg-slate-300' : null}`} onClick={handleClick}>
+          <SquareChevronLeft />
+        </span>
         :
-        <SquareChevronRight onClick={handleClick} className="cursor-pointer" />
+        <span className={`cursor-pointer transition duration-300 rounded-full w-10 h-10 flex justify-center items-center 
+        ${isOpen ? null : 'hover:bg-slate-300'}`} onClick={handleClick}>
+          <SquareChevronRight />
+        </span>
       }
+
       <div className="flex justify-center items-center flex-col gap-6 w-full">
-        <div className={`flex items-center gap-4 w-full ${isOpen ? 'justify-start' : 'justify-end'}`}>
-          <User className="cursor-pointer" />
+        <div className={`flex items-center gap-4 w-full cursor-pointer 
+          ${isOpen ? 'justify-start' : 'justify-end'}`}>
+          <span className={`cursor-pointer transition duration-300 rounded-full w-10 h-10 flex justify-center items-center 
+            ${isOpen ? null : 'hover:bg-slate-300'}`} >
+            <User />
+          </span>
           {isOpen ? <p className="text-center">USER</p> : null}
         </div>
-        <div className={`flex items-center gap-4 w-full ${isOpen ? 'justify-start' : 'justify-end'}`}>
-          <Music className="cursor-pointer" />
+        <div className={`flex items-center gap-4 w-full cursor-pointer 
+          ${isOpen ? 'justify-start' : 'justify-end'}`}>
+          <span className={`cursor-pointer transition duration-300 rounded-full w-10 h-10 flex justify-center items-center 
+            ${isOpen ? null : 'hover:bg-slate-300'}`} >
+            <Music />
+          </span>
           {isOpen ? <p className="text-center">HOT</p> : null}
         </div>
-        <div className={`flex items-center gap-4 w-full ${isOpen ? 'justify-start' : 'justify-end'}`}>
-          <Library className="cursor-pointer" />
+        <div className={`flex items-center gap-4 w-full cursor-pointer 
+          ${isOpen ? 'justify-start' : 'justify-end'}`}>
+          <span className={`cursor-pointer transition duration-300 rounded-full w-10 h-10 flex justify-center items-center 
+            ${isOpen ? null : 'hover:bg-slate-300'}`} >
+            <Library />
+          </span>
           {isOpen ? <p className="text-center">PLAYLIST</p> : null}
         </div>
       </div>
